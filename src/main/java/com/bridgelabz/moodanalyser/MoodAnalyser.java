0cb9f2504998341b9617ae386bd4f7f8e949dc41
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-03 22:43:12 +0530

    Added: Logic to find word from from string and return Happy

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -5,7 +5,6 @@ public class MoodAnalyser {
      * Method to check if mood is happy or sad Convert the message to lower case and
      * check if the string contains happy or sad word in it. Using if else return
      * Happy or Sad string.
-     *
      * @param message - String message from the main method.
      * @return - We will return the mood Happy or Sad
      */
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -9,7 +9,6 @@ public class MoodAnalyserTest {
     public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Sad Mood");
-        System.out.println(mood);
         Assertions.assertEquals("Sad", mood);
     }
 
@@ -17,6 +16,7 @@ public class MoodAnalyserTest {
     public void givenMessage_WhenHappy_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        System.out.println(mood);
         Assertions.assertEquals("Happy", mood);
     }
 }
