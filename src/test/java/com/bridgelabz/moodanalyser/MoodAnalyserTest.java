1180c248c4528923210bc330bbe0e8ca948cdce4
Shubham Raj <106942142+dvlpshubham@users.noreply.github.com>
2022-12-04 14:26:39 +0530

    Update MoodAnalyserTest.java

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -5,18 +5,25 @@ import org.junit.jupiter.api.Test;
 
 
 public class MoodAnalyserTest {
-    @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
-        System.out.println(mood);
-        Assertions.assertEquals("Sad", mood);
-    }
-
-    @Test
-    public void givenMessage_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
-        Assertions.assertEquals("Happy", mood);
-    }
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+	/**
+	 * Method to check the sad mood. Provided string with sad then method should
+	 * return sad. Used the assert equals to check if the method output is sad.
+	 * 
+	 */
+	@Test
+	public void givenMessage_IsSad_ShouldReturnSad() {
+		String mood = moodAnalyser.analyseMood("I am Sad Mood");
+		Assertions.assertEquals("Sad", mood);
+	}
+	/**
+	 * Method to check the happy mood. Provided string with happy then method should
+	 * return happy. Used the assert equals to check if the method output is happy.
+	 * 
+	 */
+	@Test
+	public void givenMessage_IsHappy_ShouldReturnHappy() {
+		String moodt = moodAnalyser.analyseMood("I am in Happy Mood");
+		Assertions.assertEquals("Happy", mood);
+	}
 }
